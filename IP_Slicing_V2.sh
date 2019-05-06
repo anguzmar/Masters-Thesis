587b70b1098d9412c28616c727960a4efc6bc68e
Angel <anguzmar@correo.ugr.es>
2019-05-05 20:37:06 -0700

    Update

--- IP_Slicing_V2.sh
+++ IP_Slicing_V2.sh
@@ -11,6 +11,8 @@ fvctl -n add-slice Regular tcp:localhost:10002 admin@RegularSlice
 fvctl -n add-flowspace dpid1_LoRa 1 100 nw_src=10.0.0.1 LoRa=7
 fvctl -n add-flowspace dpid1_LoRa 1 100 nw_dst=10.0.0.1 LoRa=7
 fvctl -n add-flowspace dpid1_Regular 1 1 any Regular=7
+fvctl -n add-flowspace dpid1_Regular_port1 1 1 in_port=1 Regular=7
+fvctl -n add-flowspace dpid1_LoRa_port2 1 1 in_port=2 LoRa=7
 
 # Add flowspaces for switch 2.
 fvctl -n add-flowspace dpid2 2 1 any Regular=7
@@ -19,6 +21,8 @@ fvctl -n add-flowspace dpid2 2 1 any Regular=7
 fvctl -n add-flowspace dpid3 3 1 any LoRa=7
 
 # Add flowspaces for switch 4.
-fvctl -n add-flowspace dpid4_LoRa 4 100 nw_src=10.0.0.3 LoRa=7
-fvctl -n add-flowspace dpid4_LoRa 4 100 nw_dst=10.0.0.3 LoRa=7
+fvctl -n add-flowspace dpid4_LoRa 4 100 nw_src=10.0.0.1 LoRa=7
+fvctl -n add-flowspace dpid4_LoRa 4 100 nw_dst=10.0.0.1 LoRa=7
 fvctl -n add-flowspace dpid4_Regular 4 1 any Regular=7
+fvctl -n add-flowspace dpid4_Regular_port1 4 1 in_port=1 Regular=7
+fvctl -n add-flowspace dpid4_LoRa_port2 4 1 in_port=2 LoRa=7

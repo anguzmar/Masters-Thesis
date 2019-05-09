4df816a9938321b938ed6192594d6ae982e0b23b
Angel <anguzmar@protonmail.com>
2019-05-09 10:06:02 +0200

    New method

--- IP_Slicing.sh
+++ IP_Slicing.sh
@@ -8,33 +8,23 @@ fvctl -n add-slice LoRa tcp:localhost:10001 admin@LoRaSlice
 fvctl -n add-slice Regular tcp:localhost:10002 admin@RegularSlice
 
 # Add flowspaces for switch 1.
-fvctl -n add-flowspace dpid1_port1 1 1 in_port=1 Regular=6
-fvctl -n add-flowspace dpid1_port2 1 1 in_port=2 LoRa=6
-
-fvctl -n add-flowspace dpid1_port3-LoRa 1 10 in_port=3,nw_src=10.0.0.1 LoRa=6
-fvctl -n add-flowspace dpid1_port3-Regular 1 1 in_port=3 Regular=6
-
-fvctl -n add-flowspace dpid1_port4-LoRa 1 10 in_port=4,nw_src=10.0.0.1 LoRa=6
-fvctl -n add-flowspace dpid1_port4-Regular 1 1 in_port=4 Regular=6
-
-fvctl -n add-flowspace dpid1_port5-LoRa 1 10 in_port=5,nw_src=10.0.0.1 LoRa=6
-fvctl -n add-flowspace dpid1_port5-Regular 1 1 in_port=5 Regular=6
-
+fvctl -n add-flowspace dpid1_LoRa_dst1 1 100 nw_dst=10.0.0.1 LoRa=7
+fvctl -n add-flowspace dpid1_LoRa_src1 1 100 nw_src=10.0.0.1 LoRa=7
+fvctl -n add-flowspace dpid1_Regular_host2_src 1 100 nw_src=10.0.0.2 Regular=7
+fvctl -n add-flowspace dpid1_Regular_host2_dst 1 100 nw_dst=10.0.0.2 Regular=7
+fvctl -n add-flowspace dpid1_Regular_port1 1 1 in_port=1 Regular=7
+fvctl -n add-flowspace dpid1_LoRa_port2 1 1 in_port=2 LoRa=7
 
 # Add flowspaces for switch 2.
-fvctl -n add-flowspace dpid2 2 1 any Regular=6
-
+fvctl -n add-flowspace dpid2 2 1 any Regular=7
 
 # Add flowspaces for switch 3.
-fvctl -n add-flowspace dpid3 3 1 any LoRa=6
-
+fvctl -n add-flowspace dpid3 3 1 any LoRa=7
 
 # Add flowspaces for switch 4.
-fvctl -n add-flowspace dpid4_port1 4 1 in_port=1 Regular=6
-fvctl -n add-flowspace dpid4_port2 4 1 in_port=2 LoRa=6
-
-fvctl -n add-flowspace dpid4_port3-LoRa 4 10 in_port=3,nw_src=10.0.0.3 LoRa=6
-fvctl -n add-flowspace dpid4_port3-Regular 4 1 in_port=3 Regular=6
-
-fvctl -n add-flowspace dpid4_port4-LoRa 4 10 in_port=4,nw_src=10.0.0.3 LoRa=6
-fvctl -n add-flowspace dpid4_port4-Regular 4 1 in_port=4 Regular=6
+fvctl -n add-flowspace dpid4_LoRa_dst3 4 100 nw_dst=10.0.0.3 LoRa=7
+fvctl -n add-flowspace dpid4_LoRa_src3 4 100 nw_src=10.0.0.3 LoRa=7
+fvctl -n add-flowspace dpid4_Regular_host4_src 4 100 nw_src=10.0.0.4 Regular=7
+fvctl -n add-flowspace dpid4_Regular_host4_dst 4 100 nw_dst=10.0.0.4 Regular=7
+fvctl -n add-flowspace dpid4_Regular_port1 4 1 in_port=1 Regular=7
+fvctl -n add-flowspace dpid4_LoRa_port2 4 1 in_port=2 LoRa=7

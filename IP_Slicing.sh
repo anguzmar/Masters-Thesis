2b30fdec6ac27adccca598766bf47121b0e31f2b
Angel <anguzmar@correo.ugr.es>
2019-05-16 14:22:54 -0700

    It works! (for now)

--- IP_Slicing.sh
+++ IP_Slicing.sh
@@ -7,7 +7,6 @@ fvctl -n add-slice DevNull tcp:localhost:666 admin@DevNull
 
 
 # Add flowspaces for switch 1.
-fvctl -n add-flowspace dpid1_ARP 1 300 dl_type=0x0806 Regular=6
 fvctl -n add-flowspace dpid1_LoRa 1 100 nw_src=10.0.0.0/24 LoRa=6
 
 fvctl -n add-flowspace dpid1_DevNull_LoRa2Regular 1 200 nw_src=10.0.0.0/24,nw_dst=192.168.0.0/24 DevNull=2
@@ -15,25 +14,25 @@ fvctl -n add-flowspace dpid1_DevNull_Regular2LoRa 1 200 nw_dst=192.168.0.0/24,nw
 
 fvctl -n add-flowspace dpid1_default 1 1 any Regular=6
 
+fvctl -n add-flowspace dpid1_DevNull_port1_ARP 1 300 in_port=1,nw_src=10.0.0.0/24,dl_type=0x0806 DevNull=6
 fvctl -n add-flowspace dpid1_Regular_port1 1 1 in_port=1 Regular=6
 
-fvctl -n add-flowspace dpid1_port2_ARP 1 300 in_port=2,dl_type=0x0806 DevNull=6
-fvctl -n add-flowspace dpid1_LoRa_port2_LoRa 1 100 in_port=2,nw_src=10.0.0.0/24 LoRa=6
-fvctl -n add-flowspace dpid1_LoRa_port2 1 1 in_port=2 DevNull=2
+fvctl -n add-flowspace dpid1_LoRa_port2_LoRa 1 200 in_port=2,nw_src=10.0.0.0/24 LoRa=6
+fvctl -n add-flowspace dpid1_DevNull_port2 1 1 in_port=2 DevNull=2
 
 
 # Add flowspaces for switch 2.
+fvctl -n add-flowspace dpid2_DevNull_ARP 2 300 nw_src=10.0.0.0/24,dl_type=0x0806 DevNull=6
 fvctl -n add-flowspace dpid2 2 1 any Regular=6
 
 
 # Add flowspaces for switch 3.
-fvctl -n add-flowspace dpid3_h1 3 100 nw_src=10.0.0.0/24 LoRa=6
-fvctl -n add-flowspace dpid3_ARP 1 300 dl_type=0x0806 DevNull=6
-fvctl -n add-flowspace dpid3_drop 3 1 any DevNull=2
+fvctl -n add-flowspace dpid3_DevNull_ARP 3 300 nw_src=192.168.0.0/24,dl_type=0x0806 DevNull=6
+fvctl -n add-flowspace dpid3 3 100 nw_src=10.0.0.0/24 LoRa=6
+fvctl -n add-flowspace dpid3_DevNull 3 1 any DevNull=2
 
 
 # Add flowspaces for switch 4.
-fvctl -n add-flowspace dpid4_ARP 1 300 dl_type=0x0806 Regular=6
 fvctl -n add-flowspace dpid4_LoRa 4 100 nw_src=10.0.0.0/24 LoRa=6
 
 fvctl -n add-flowspace dpid4_DevNull_LoRa2Regular 4 200 nw_src=10.0.0.0/24,nw_dst=192.168.0.0/24 DevNull=2
@@ -41,7 +40,8 @@ fvctl -n add-flowspace dpid4_DevNull_Regular2LoRa 4 200 nw_dst=192.168.0.0/24,nw
 
 fvctl -n add-flowspace dpid4_default 4 1 any Regular=6
 
+fvctl -n add-flowspace dpid4_DevNull_port1_ARP 4 300 in_port=1,nw_src=10.0.0.0/24,dl_type=0x0806 DevNull=6
 fvctl -n add-flowspace dpid4_Regular_port4 4 1 in_port=1 Regular=6
 
-fvctl -n add-flowspace dpid4_LoRa_port2_LoRa 4 100 in_port=2,nw_src=10.0.0.0/24 LoRa=6
-fvctl -n add-flowspace dpid4_LoRa_port2 4 1 in_port=2 DevNull=2
+fvctl -n add-flowspace dpid4_LoRa_port2_LoRa 4 200 in_port=2,nw_src=10.0.0.0/24 LoRa=6
+fvctl -n add-flowspace dpid4_DevNull_port2 4 1 in_port=2 DevNull=2
